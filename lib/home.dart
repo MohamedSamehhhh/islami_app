63cf0ac071a7aeda2807f267c579dddd88f96791
MohamedSamehhhh <mohamedsameh7222@gmail.com>
2023-09-14 20:56:11 +0300

    initial app with empty home screen

---
+++ lib/home.dart
@@ -0,0 +1,15 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('hello'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
